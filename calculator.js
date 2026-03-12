8491117a41dc5f9c9cd62053d94492f32cff127e
NotNate10 <buleyanate@gmail.com>
2026-03-12 12:09:15 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -5,7 +5,7 @@ allButtonElements.forEach((button) => {
   const buttonChar = button.innerHTML
   
   button.addEventListener('click', () => {
-    if (buttonChar !== 'C' && buttonChar !== 'X' && buttonChar !== '=') {
+    if (buttonChar !== 'C' && buttonChar !== 'Delete' && buttonChar !== '=') {
       if (displayAreaElement.value === '0' && buttonChar === '0') {
         displayAreaElement.value = '0'
       } else if (displayAreaElement.value === '.' && buttonChar === '.') {
@@ -29,7 +29,7 @@ allButtonElements.forEach((button) => {
       } else {
         displayAreaElement.value = 'Error';
       }
-    } else if (buttonChar === 'X') {
+    } else if (buttonChar === 'Delete') {
       displayAreaElement.value = displayAreaElement.value.slice(0, -1);
     }
   })
