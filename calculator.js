88f1abd0cf5f95a857d0b35855201bbf6567163b
NotNate10 <buleyanate@gmail.com>
2026-03-12 12:07:14 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,34 +1,18 @@
 const displayAreaElement = document.querySelector('.js-display-area');
 const allButtonElements = document.querySelectorAll('button');
-const bodyElement = document.body;
 
 allButtonElements.forEach((button) => {
   const buttonChar = button.innerHTML
-  bodyElement.addEventListener('keydown', (event) => {
-    if (event.key === buttonChar && buttonChar !== 'C' && buttonChar !== 'X' && buttonChar !== '=' && buttonChar !== '.') {
-      displayAreaElement.value += buttonChar;
-    } else if (event.key === 'Backspace') {
-      displayAreaElement.value = '';
-    } else if (event.key === '=' || event.key === 'Enter') {
-      let result;
-
-      try {
-        result = eval(displayAreaElement.value);
-      } catch (error) {
-        displayAreaElement.value = 'Error'
-      }
-      
-      if (result !== NaN && result !== undefined) {
-        displayAreaElement.value = result;
-      } else {
-        displayAreaElement.value = 'Error';
-      }
-    }
-  })
   
   button.addEventListener('click', () => {
     if (buttonChar !== 'C' && buttonChar !== 'X' && buttonChar !== '=') {
-      displayAreaElement.value += buttonChar;
+      if (displayAreaElement.value === '0' && buttonChar === '0') {
+        displayAreaElement.value = '0'
+      } else if (displayAreaElement.value === '.' && buttonChar === '.') {
+        displayAreaElement.value = '.'
+      } else {
+        displayAreaElement.value += buttonChar;
+      }
     } else if (buttonChar === 'C') {
       displayAreaElement.value = '';
     } else if (buttonChar === '=') {
